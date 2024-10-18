c699ceb41ed8aa6f4efeed205c49d9b9497ab080
Coding for English Teacher <learnbidoing@gmail.com>
2024-10-19 02:34:07 +0900

    Update classapp.py

--- pages/classapp.py
+++ pages/classapp.py
@@ -77,7 +77,7 @@ with tabs[0]:
 
 # Timer tab
 with tabs[1]:
-    st.text("⏳ MK316 Timer")
+    st.text("🐾 MK316 Timer")
 
     # Initialize session state for countdown
     if "countdown_started" not in st.session_state:
